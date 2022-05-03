50ad29dbfe89da377fb863f396166a427d1c132e
kickylau <kickyliu2018@gmail.com>
2022-05-03 16:53:29 +0000

    event and note backend post finished

--- app/models/events.py
+++ app/models/events.py
@@ -1,7 +1,39 @@
 from .db import db
+from datetime import datetime
+
 
 
 class Event(db.Model):
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(4000), nullable=False)
+    image_url = db.Column(db.String(510), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+
+
+    #not sure the relationship with trip
+   # events = db.relationship("Event", back_populates=("trip"))
+
+    @property
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "ownerId": self.owner_id,
+            "tripId":self.trip_id,
+            "name": self.name,
+            "description": self.description,
+            "imageUrl": self.image_url,
+            "location": self.location,
+            "startDate": self.start_date,
+            "endDate": self.end_date,
+            "createdAt": self.created_at,
+            "updatedAt": self.updated_at,
+        }
--- app/models/notes.py
+++ app/models/notes.py
@@ -1,7 +1,29 @@
 from .db import db
+from datetime import datetime
 
 
 class Note(db.Model):
     __tablename__ = 'notes'
 
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, nullable=False)
+    trip_id = db.Column(db.Integer, nullable=False)
+    trip_date = db.Column(db.Date, nullable=False)
+    note = db.Column(db.String(1000), nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+
+
+#relations?
+
+    @property
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "ownerId": self.owner_id,
+            "tripId":self.trip_id,
+            "tripDate": self.trip_date,
+            "note": self.note,
+            "createdAt": self.created_at,
+            "updatedAt": self.updated_at,
+        }
