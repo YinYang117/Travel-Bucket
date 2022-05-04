1ed3be90bcc7c5cd55d76e5ba368be2996f3e07b
kickylau <kickyliu2018@gmail.com>
2022-05-04 20:53:57 +0000

    testing post event

--- react-app/src/components/EventModal/EventForm.js
+++ react-app/src/components/EventModal/EventForm.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import * as eventActions from "../../store/events";
+import * as eventActions from "../../store/event";
 import { useHistory } from "react-router-dom";
+import { Modal } from "../../context/Modal"
 
 
 function Event() {
     const dispatch = useDispatch();
     const history = useHistory();
     const sessionUser = useSelector(state => state.session.user);
-    const eventsObj = useSelector(state => state.eventss)
+    const eventsObj = useSelector(state => state.events)
     const events = Object.values(eventsObj)
 
 
     const [ownerId, setOwnerId] = useState(sessionUser?.id);
     const [name, setName] = useState("");
-    const [destination, setDestination] = useState("");
+    const [description, setDescription] = useState("");
+    const [destionation, setDestination] = useState("");
+    const [location, setLocation] = useState("");
     const [imageUrl, setImageUrl] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
     const [errors, setErrors] = useState([]);
+    const [tripId, setTripId] = useState("");
+    const [showModal, setShowModal] = useState(false);
+
 
     useEffect(() => {
         if (!sessionUser) history.push('/')
@@ -37,29 +43,63 @@ function Event() {
         newEventData.location = location
         newEventData.startDate = startDate
         newEventData.endDate = endDate
-
+       
+        console.log("NEW EVENT DATA FROM MODAL --------------------", newEventData)
         dispatch(eventActions.newEvent(newEventData))
 
     };
 
 
-    const deleteEvent = () => {
-        setErrors([]);
-
-        dispatch(eventActions.deleteEvent(event.id))
-            .catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            });
-    }
-
 
     return (
         <>
-            <h1> All Events </h1>
-            {events}
-
+          <button className="AddAEventButton" onClick={() => setShowModal(true)}>
+            {/* <i class="fa-solid fa-right-to-bracket"></i> */}
+            Add An Event
+          </button>
+          {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+              <div className="formContainer">
+                <h1> Add An Event </h1>
+                <form
+                  className="new-event-form"
+                  onSubmit={e => {
+                    e.preventDefault();
+                    submitNewEvent();
+                  }}>
+                  <label className='label'>
+                    Event Name:
+                  </label>
+                  <input onChange={e => setName(e.target.value)} type="text" className="new-event-name" placeholder='Event Name' value={name} />
+                  <label className='label'>
+                    Event Description:
+                  </label>
+                  <input onChange={e => setDescription(e.target.value)} type="text" className="new-event-description" placeholder='Event Description' value={description} />
+                  <label className='label'>
+                    Event Location:
+                  </label>
+                  <input onChange={e => setLocation(e.target.value)} type="text" className="new-event-location" placeholder='Event Location' value={location} />
+                  <label className='label'>
+                    Event Image URL:
+                  </label>
+                  <input onChange={e => setImageUrl(e.target.value)} type="text" className="new-event-image" placeholder='Image Url' value={imageUrl} />
+                  <label className='label'>
+                    Event Start:
+                  </label>
+                  <input onChange={e => setStartDate(e.target.value)} type="date" className="new-event-start-date" value={startDate} />
+                  <label className='label'>
+                    Event End:
+                  </label>
+                  <input onChange={e => setEndDate(e.target.value)} type="date" className="new-event-end-date" value={endDate} />
+                  <ul className="new-event-errors">
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                  </ul>
+                  <button className="new-event-submit" type='submit' >Submit New event</button>
+                </form>
+              </div>
+            </Modal>
+          )}
         </>
-    );
+      );
 }
 export default Event;
--- react-app/src/components/EventModal/index.js
+++ react-app/src/components/EventModal/index.js
@@ -3,21 +3,26 @@ import { Modal } from "../../context/Modal";
 import EventForm from "./EventForm";
 
 function EventFormModal() {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      <button className="EventButton" onClick={() => setShowModal(true)}>
-        {/* <i class="fa-solid fa-right-to-bracket"></i> */}
-        Add a Event
-      </button>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <EventForm />
-        </Modal>
-      )}
-    </>
-  );
+    return (
+        <>
+            <h1> All Events </h1>
+            <h1> All Events </h1>
+            <h1> All Events </h1>
+            <h1> All Events </h1>
+            <h1> All Events </h1>
+            <button className="EventButton" onClick={() => setShowModal(true)}>
+                {/* <i class="fa-solid fa-right-to-bracket"></i> */}
+                Add a Event
+            </button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <EventForm />
+                </Modal>
+            )}
+        </>
+    );
 }
 
 export default EventFormModal;
