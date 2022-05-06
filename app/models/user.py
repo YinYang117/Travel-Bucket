cf2ebca8a11d1b427d4df81cfc6cbabb263231e0
Ryan Schneider <slcryan2010@yahoo.com>
2022-05-05 20:18:32 -0600

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,6 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from .trip_invites import trip_invites
-# from .trips import Trip 
 
 
 class User(db.Model, UserMixin):
