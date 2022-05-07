a9c5dd22f663ebeb0f84f54473dc5866165059d2
Huyen Nguyen <hn078722@gmail.com>
2022-05-07 21:50:21 +0000

    final for the notes

--- react-app/src/components/NoteModal/DeleteNoteForm.js
+++ react-app/src/components/NoteModal/DeleteNoteForm.js
@@ -9,12 +9,12 @@ function DeleteNote({ hideModal, note }) {
 
     const handleSubmit = async (e, note) => {
         e.preventDefault();
-        // setErrors([]);
+        setErrors([]);
         console.log("THIS IS NOTE-------->", note)
         dispatch(removeNote(note.id))
             .catch(async (res) => {
                 const data = await res.json();
-                // if (data && data.errors) setErrors(data.errors);
+                if (data && data.errors) setErrors(data.errors);
             });
     }
 
