ed14af6f370584c91620fd0ee48cbdf84e432c05
YinYang117 <slcryan2010@yahoo.com>
2022-05-07 13:03:33 -0600

    bout to change models for user,trip,tripInvites

--- app/models/trip_invites.py
+++ app/models/trip_invites.py
@@ -1,12 +1,7 @@
 from .db import db
 
 
-trip_invites = db.Table(
-    "trip_invites",
-    db.Column(
-        "user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True
-    ),
-    db.Column(
-        "trip_id", db.Integer, db.ForeignKey("trips.id"), primary_key=True
-    )
+trip_invites = db.Table("trip_invites",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("trip_id", db.Integer, db.ForeignKey("trips.id"), primary_key=True)
 )
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .invited_users import seed_invited_users, undo_invited_users
 from .trips import seed_trips, undo_trips
+from .invited_users import seed_invited_users, undo_invited_users
+
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -20,7 +21,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_trips()
+    
     undo_invited_users()
+    undo_trips()
+    undo_users()
     # Add other undo functions here
