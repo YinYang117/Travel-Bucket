7ff4ab9f74a60e38523771aa7f0961dd2622d754
Huyen Nguyen <hn078722@gmail.com>
2022-05-08 19:08:03 +0000

    still working...

--- app/api/trip_routes.py
+++ app/api/trip_routes.py
@@ -51,28 +51,28 @@ def trips():
 
 @trip_routes.route("/users/<int:id>")
 def users_owned_trips(id):
-    trips = Trip.query.filter(Trip.owner_id == id).all()
-    user = User.query.get(id)
-    invited_trips = user.invited_trips
+    # trips = Trip.query.filter(Trip.owner_id == id).all()
+    # user = User.query.get(id)
+    # invited_trips = user.invited_trips
 
-    if trips and invited_trips:
-        made_trips = {}
-        other_trips = {}
-        for trip in trips:
-            made_trips[trip.id] = trip.to_dict
-        for invited_trip in invited_trips:
-            other_trips[invited_trip.id] = invited_trip.to_dict
-        return {**made_trips, **other_trips}
+    # if trips and invited_trips:
+    #     made_trips = {}
+    #     other_trips = {}
+    #     for trip in trips:
+    #         made_trips[trip.id] = trip.to_dict
+    #     for invited_trip in invited_trips:
+    #         other_trips[invited_trip.id] = invited_trip.to_dict
+    #     return {**made_trips, **other_trips}
     # else:
     #     return {'error': ['No Trips found for this User']}
 
-    if trips and not invited_trips:
-        made_trips = {}
-        for trip in trips:
-            made_trips[trip.id] = trip.to_dict
-        return made_trips
-    else:
-        return {'error': ['No Trips found for this User']}
+    # if not invited_trips:
+    #     made_trips = {}
+    #     for trip in trips:
+    #         made_trips[trip.id] = trip.to_dict
+    #     return made_trips
+    # else:
+    #     return {'error': ['No Trips found for this User']}
 
 
 @trip_routes.route("/<int:id>", methods=["GET", "PUT", "DELETE"])
--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -27,40 +27,40 @@ function Home() {
 
         // console.log("THIS IS INvited USERS-----------", invitedUsers)
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const response = await fetch(`/api/invited_users/${sessionUser.id}/trips`);
-    //         const responseData = await response.json();
-    //         const allTrips = Object.values(responseData)
-    //         console.log("THIS IS ALL OF THE TRIPS THAT EXISTS-------------", allTrips)
-    //         // console.log("THIS IS ALL OF THE TRIPS THAT EXISTS-------------", responseData)
-    //         setinvitedUserTrips(allTrips);
-    //     }
-    //     fetchData();
-    //     }, []);
-    
-    // useEffect(() => {
-    //     if(sessionUser.id) {
-    //         dispatch(invitedUsersActions.loadInvitedUsers(sessionUser.id))
-    //     }
-    // }, [sessionUser.id])
+        
+        // useEffect(() => {
+            //     if(sessionUser.id) {
+                //         dispatch(invitedUsersActions.loadInvitedUsers(sessionUser.id))
+                //     }
+                // }, [sessionUser.id])
+                
+                
+                
+                // console.log("THIS IS INVITED USER TRIPS IN THE FRONT END-----------------------", invitedUsers)
+                
+                // const invitedUsersObj = useSelector(state => state.invited)
+                // const invitedUsers = Object.values(invitedUsersObj)
+                
+                // const singleInvitedUser = invitedUsers.filter(invitedUser => invitedUser.id === sessionUser.id) 
+                
+                // have a store and backend route for all trips 
+                // check id of trip with id of session
+                // query in the backend user, get the id then user.invited_trips
+                // then get all trips 
+                // then display the trips
+                
+        // useEffect(() => {
+        //     async function fetchData() {
+        //         const response = await fetch(`/api/invited_users/${sessionUser.id}/trips`);
+        //         const responseData = await response.json();
+        //         const allTrips = Object.values(responseData)
+        //         console.log("THIS IS ALL OF THE TRIPS THAT EXISTS-------------", allTrips)
+        //         // console.log("THIS IS ALL OF THE TRIPS THAT EXISTS-------------", responseData)
+        //         setinvitedUserTrips(allTrips);
+        //     }
+        //     fetchData();
+        //     }, []);
 
-    
-
-    // console.log("THIS IS INVITED USER TRIPS IN THE FRONT END-----------------------", invitedUsers)
-
-    // const invitedUsersObj = useSelector(state => state.invited)
-    // const invitedUsers = Object.values(invitedUsersObj)
-
-    // const singleInvitedUser = invitedUsers.filter(invitedUser => invitedUser.id === sessionUser.id) 
-    
-        // have a store and backend route for all trips 
-        // check id of trip with id of session
-        // query in the backend user, get the id then user.invited_trips
-        // then get all trips 
-        // then display the trips
-
-    
     useEffect(() => {
         if (!sessionUser) history.push('/')
         if (sessionUser) dispatch(tripActions.loadAllUserRelatedTrips(sessionUser.id))
@@ -71,10 +71,10 @@ function Home() {
             <h1 id="all-trips"> All Trips </h1>
             <div className="trip-gallery">
                 {/* {invitedUserTrips && invitedUserTrips.map(invitedUserTrip => 
-                        <TripCard key={invitedUserTrip.id} trip={invitedUsertrip} />
-                    // <li key={invitedUserTrip.id}>  
-                    //     {invitedUserTrip.name}
-                    // </li>
+                    // <TripCard key={invitedUserTrip.id} trip={invitedUsertrip} />
+                    <li key={invitedUserTrip.id}>  
+                        {invitedUserTrip.name}
+                    </li>
                 )} */}
                 {trips &&
                 trips.map(trip =>
