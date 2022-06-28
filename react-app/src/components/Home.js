d5fcabb19aafc10f0b7a013880eeec208658de0a
YinYang117 <slcryan2010@yahoo.com>
2022-06-28 13:53:48 -0600

    removed unused paths from early code

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -14,7 +14,6 @@ function Home() {
     const sessionUser = useSelector(state => state.session.user);
     const tripsObj = useSelector(state => state.trips)
     const trips = Object.values(tripsObj)
-    console.log("THIS IS ALL==========TRIPS=============", trips)
     const [invitedUserTrips, setinvitedUserTrips] = useState();
 
     
@@ -22,34 +21,26 @@ function Home() {
         if (!sessionUser) history.push('/')
         if (sessionUser) dispatch(tripActions.loadAllUserRelatedTrips(sessionUser.id))
     }, [sessionUser])
-    
-
-
-//   const libraries = ["places"];
+    //   const libraries = ["places"];
 
     return (
         <>
-                <>
-                <div className="page-container">
-                    <h1 id="all-trips"> All Trips </h1>
-                    <div className="trip-gallery">
-
-                        {trips && trips?.map(trip =>
-                            <TripCard key={trip?.id} trip={trip} />
-                        )}
-                        {(trips?.length === 0) &&
+            <div className="page-container">
+                <h1 id="all-trips"> All Trips </h1>
+                <div className="trip-gallery">
+                    {trips && trips?.map(trip =>
+                        <TripCard key={trip?.id} trip={trip} />
+                    )}
+                    {(trips?.length === 0) &&
                         <div className="trip-container">
                             <h3 id="no-trip">Plan a Trip Now!</h3>
                             <img id="trip-image" src="/static/travel.png" alt="Where to?" className="image"/>
                             <AddATripModal />
                         </div>
-                        }
-                    </div>
+                    }
                 </div>
-                </>
+            </div>
         </>
-       
-    
     );
 }
 
